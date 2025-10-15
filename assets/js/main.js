dc780f943dab6058307ff8f85a6e935334b496c2
gabrielemarchesano <marchesano.gabriele@gmail.com>
2025-10-15 17:31:29 +0200

    milestone2: create html element to put the input values on the page

--- assets/js/main.js
+++ assets/js/main.js
@@ -69,7 +69,39 @@ form.addEventListener("submit", (event) => {
     const ticket = ticketPrice(kmValue, ticketTariff);
     console.log(`Costo del biglietto: ${ticket}€`);    
   }
-  
 })
 
-//va applicato uno sconto del 40% per gli over 65
+
+/* ========== MILESTONE 2 ========== */
+/* 
+Solo una volta che il milestone 1 sarà completo e funzionante allora realizzeremo un form in pagina in cui l’utente potrà inserire i dati e visualizzare il calcolo finale con il prezzo. 
+Il recap dei dati e l'output del prezzo finale, andranno quindi stampati in pagina (il prezzo dovrà essere formattato con massimo due decimali, per indicare i centesimi sul prezzo). Questo richiederà un minimo di ricerca.
+*/
+
+const infoElement = document.getElementById("travel-info");
+let message;
+
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+  //Creo le variabili dove salvare il valore inserito negli input
+  const ageValue = ageElement.value;
+  const kmValue = kmElement.value;
+
+  
+  //Controllo se l'utente è minorenne
+  if(ageValue < 18){
+    //Se lo è, applico lo sconto del 20%
+    const under18Ticket = ticketPrice(kmValue, ticketTariff) * 0.80;
+    message = `Hai: ${ageValue} anni; Percorrerai: ${kmValue}km; Prezzo del biglietto: ${under18Ticket.toFixed(2)}€`;
+  } else if(ageValue > 65){ //Altrimenti se è over 65
+    //Applico lo sconto del 60%
+    const over65Ticket = ticketPrice(kmValue, ticketTariff) * 0.60;
+    message = `Hai: ${ageValue} anni; Percorrerai: ${kmValue}km; Prezzo del biglietto: ${over65Ticket.toFixed(2)}€`;
+  } else{ //Altrimenti
+    //Non si applicano sconti
+    const ticket = ticketPrice(kmValue, ticketTariff);
+    message = `Hai: ${ageValue} anni; Percorrerai: ${kmValue}km; Prezzo del biglietto: ${ticket.toFixed(2)}€`;
+  }
+  
+  infoElement.innerHTML = message;
+})
